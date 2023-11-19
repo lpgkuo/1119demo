1ab550ecd4db385955aedb717481f97ac51cabc7
lpgkuo <151419360+lpgkuo@users.noreply.github.com>
2023-11-19 13:37:05 +0800

    Create main.cpp
    main code

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x=5;	 
+	#ifdef __DEBUG
+		cerr<<"x="<<x<<endl;
+		asm("int $3");
+	#endif
+	#ifdef __ADD
+		asm("add $1,%1":"=r"(x):"r"(x));
+	#endif
+	x+=6;
+	cout <<x<<endl;
+	return 1;
+}
